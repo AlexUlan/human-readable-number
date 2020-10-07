3a9c1e9002a2c2e96eed62ed7440bc5f7c1669f9
AlexUlan <alexander33.xata@gmail.com>
2020-10-07 09:21:23 +0200

    feat: implement returns number to string

--- src/index.js
+++ src/index.js
@@ -1,144 +1,153 @@
 module.exports = function toReadable(number) {
-    let toReadString;
-
     let readString;
+    let numberString = number.toString();
 
-  function nameDigit(digit) {
-    let stringdigit;
-    switch (digit) {
-      case "0":
-        stringdigit = "zero";
-        break;
-      case "1":
-        stringdigit = "one";
-        break;
-      case "2":
-        stringdigit = "two";
-        break;
-      case "3":
-        stringdigit = "three";
-        break;
-      case "4":
-        stringdigit = "four";
-        break;
-      case "5":
-        stringdigit = "five";
-        break;
-      case "6":
-        stringdigit = "six";
-        break;
-      case "7":
-        stringdigit = "seven";
-        break;
-      case "8":
-        stringdigit = "eight";
-        break;
-      case "9":
-        stringdigit = "nine";
-        break;
+    function nameDigit(digit) {
+        let stringdigit;
+        switch (digit) {
+            case "0":
+                stringdigit = "zero";
+                break;
+            case "1":
+                stringdigit = "one";
+                break;
+            case "2":
+                stringdigit = "two";
+                break;
+            case "3":
+                stringdigit = "three";
+                break;
+            case "4":
+                stringdigit = "four";
+                break;
+            case "5":
+                stringdigit = "five";
+                break;
+            case "6":
+                stringdigit = "six";
+                break;
+            case "7":
+                stringdigit = "seven";
+                break;
+            case "8":
+                stringdigit = "eight";
+                break;
+            case "9":
+                stringdigit = "nine";
+                break;
 
-      default:
-        break;
+            default:
+                break;
+        }
+
+        return stringdigit;
     }
 
-    return stringdigit;
-  }
+    function nameNumbersUntilTwenty(number) {
+        let stringNumber;
+        switch (number) {
+            case "10":
+                stringNumber = "ten";
+                break;
+            case "11":
+                stringNumber = "eleven";
+                break;
+            case "12":
+                stringNumber = "twelve";
+                break;
+            case "13":
+                stringNumber = "thirteen";
+                break;
+            case "14":
+                stringNumber = "fourteen";
+                break;
+            case "15":
+                stringNumber = "fifteen";
+                break;
+            case "16":
+                stringNumber = "sixteen";
+                break;
+            case "17":
+                stringNumber = "seventeen";
+                break;
+            case "18":
+                stringNumber = "eighteen";
+                break;
+            case "19":
+                stringNumber = "nineteen";
+                break;
 
-  function nameNumbersUntilTwenty(number) {
-    let stringNumber;
-    switch (number) {
-      case "10":
-        stringNumber = "ten";
-        break;
-      case "11":
-        stringNumber = "eleven";
-        break;
-      case "12":
-        stringNumber = "twelve";
-        break;
-      case "13":
-        stringNumber = "thirteen";
-        break;
-      case "14":
-        stringNumber = "fourteen";
-        break;
-      case "15":
-        stringNumber = "fifteen";
-        break;
-      case "16":
-        stringNumber = "sixteen";
-        break;
-      case "17":
-        stringNumber = "seventeen";
-        break;
-      case "18":
-        stringNumber = "eighteen";
-        break;
-      case "19":
-        stringNumber = "nineteen";
-        break;
+            default:
+                break;
+        }
 
-      default:
-        break;
+        return stringNumber;
     }
 
-    return stringNumber;
-  }
+    function nameNumbersDozens(number) {
+        let stringnumber;
+        switch (number) {
+            case "2":
+                stringnumber = "twenty";
+                break;
+            case "3":
+                stringnumber = "thirty";
+                break;
+            case "4":
+                stringnumber = "forty";
+                break;
+            case "5":
+                stringnumber = "fifty";
+                break;
+            case "6":
+                stringnumber = "sixty";
+                break;
+            case "7":
+                stringnumber = "seventy";
+                break;
+            case "8":
+                stringnumber = "eighty";
+                break;
+            case "9":
+                stringnumber = "ninety";
+                break;
+            default:
+                break;
+        }
 
-  function nameNumbersDozens(number) {
-    let stringnumber;
-    switch (number) {
-      case "2":
-        stringdigit = "twenty";
-        break;
-      case "3":
-        stringdigit = "thirty";
-        break;
-      case "4":
-        stringdigit = "forty";
-        break;
-      case "5":
-        stringdigit = "fifty";
-        break;
-      case "6":
-        stringdigit = "sixty";
-        break;
-      case "7":
-        stringdigit = "seventy";
-        break;
-      case "8":
-        stringdigit = "eighty";
-        break;
-      case "9":
-        stringdigit = "ninety";
-        break;
-      default:
-        break;
+        return stringnumber;
     }
 
-    return stringdigit;
-  }
+    function nameHundred(hundred) {
+        return nameDigit(hundred) + " hundred";
+    }
+    if (numberString.length === 1) {
+        return nameDigit(numberString);
+    }
 
-  function nameHundred(hundred) {
-    return nameDigit(hundred) + " hundred";
-  }
+    if (number >= 10 && number < 20) {
+        return nameNumbersUntilTwenty(numberString);
+    }
 
-  let numberArray = number.toString().split("");
+    if (number >= 20 && number < 100) {
+        readString =
+            +numberString.slice(1) === 0
+                ? `${nameNumbersDozens(numberString[0])}`
+                : `${nameNumbersDozens(numberString[0])} ${nameDigit(
+                      numberString.slice(1)
+                  )}`;
+        return readString;
+    }
 
-  if (numberArray.length === 1) {
-    return nameDigit(number);
-  }
+    if (number >= 100 && number < 1000) {
+        readString =
+            +numberString.slice(1) === 0
+                ? `${nameHundred(numberString[0])}`
+                : `${nameHundred(numberString[0])} ${toReadable(
+                      parseInt(numberString.slice(1), 10).toString()
+                  )}`;
+        return readString;
+    }
 
-  if (number >= 10 && number < 20) {
-    return nameNumbersUntilTwenty(number);
-  }
-
-  if (number >= 20 && number < 100) {
-    for (let i = numberArray.length; i < 0; i--) {}
-    return nameNumbersUntilTwenty(number);
-  }
-
-  /*   toReadString = nameDigit(number); */
-  toReadString = nameHundred(number);
-  return toReadString;
+    return readString;
 };
